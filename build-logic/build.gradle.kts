068d1b3e93349280e48a6b1d4fb3578a8c8e37a2
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-07-10 09:28:18 -0700

    Add helium dependencies and update submodules
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -19,4 +19,18 @@ dependencies {
     implementation(libs.kotlin.gradle.plugin)
     implementation(libs.github.johnrengelman.shadow)
     implementation(libs.nemerosa.versioning)
+
+    api(libs.com.wire.xenon)
+    api(libs.jakarta.ws.rs.jakarta.ws.rs.api)
+    api(libs.org.glassfish.tyrus.bundles.tyrus.standalone.client)
+    api(libs.org.glassfish.jersey.core.jersey.client)
+    api(libs.org.flywaydb.flyway.core)
+    api(libs.org.glassfish.jersey.inject.jersey.hk2)
+    api(libs.com.fasterxml.jackson.jaxrs.jackson.jaxrs.json.provider)
+    testImplementation(libs.org.slf4j.slf4j.api)
+    testImplementation(libs.org.slf4j.slf4j.log4j12)
+    testImplementation(libs.javax.activation.activation)
+    testImplementation(libs.org.postgresql.postgresql)
+    testImplementation(libs.org.junit.jupiter.junit.jupiter)
+    testImplementation(libs.org.junit.jupiter.junit.jupiter.engine)
 }
